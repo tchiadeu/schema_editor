065ed23b360a96dfa9cbd41d6b74c7485e2412d8
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-05 13:22:25 +0200

    test: adapt fixtures to match possibilites

--- spec/fixtures/schema.rb
+++ spec/fixtures/schema.rb
@@ -1,3 +1,11 @@
+create_table "posts", force: :cascade do |t|
+  t.integer "id"
+  t.string "title"
+  t.text "body"
+  t.integer "user_id"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
 create_table "users", force: :cascade do |t|
   t.integer "id"
   t.string "name"
@@ -7,7 +15,23 @@ create_table "users", force: :cascade do |t|
   t.datetime "updated_at", null: false
 end
 
-create_table "posts", force: :cascade do |t|
+create_table "comments", force: :cascade do |t|
+  t.integer "id"
+  t.string "title"
+  t.text "body"
+  t.integer "post_id"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+create_table "articles", force: :cascade do |t|
+  t.integer "id"
+  t.string "title"
+  t.text "body"
+  t.integer "user_id"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+create_table "tests", force: :cascade do |t|
   t.integer "id"
   t.string "title"
   t.text "body"
@@ -17,3 +41,6 @@ create_table "posts", force: :cascade do |t|
 end
 
 add_foreign_key "posts", "users"
+add_foreign_key "comments", "posts"
+add_foreign_key "articles", "users"
+add_foreign_key "tests", "users"
