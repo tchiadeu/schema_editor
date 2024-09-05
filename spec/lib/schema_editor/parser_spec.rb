f000890cf9715f1eaf1861c3b2218dbc8a63fad7
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-05 13:22:11 +0200

    test: add test for ordering tables

--- spec/lib/schema_editor/parser_spec.rb
+++ spec/lib/schema_editor/parser_spec.rb
@@ -9,14 +9,22 @@ RSpec.describe SchemaEditor::Parser do
   describe '.parse_tables' do
     it "should have the good keys" do
       result = SchemaEditor::Parser.send(:parse_tables, file)
-      expect(result.keys).to eq(%w[users posts])
+      expect(result.keys.sort).to eq(%w[users posts comments articles tests].sort)
     end
   end
 
   describe '.parse_foreign_keys' do
     it "should have the good foreign keys" do
       result = SchemaEditor::Parser.send(:parse_foreign_keys, file)
-      expect(result).to eq({ 'posts' => [ 'users' ] })
+      expect(result).to eq({ 'users' => 3, 'posts' => 1 })
+    end
+  end
+
+  describe '.extract' do
+    it 'should order the hash by reference occurrences' do
+      result = SchemaEditor::Parser.extract(file)
+      expect(result.keys[0]).to eq('users')
+      expect(result.keys[1]).to eq('posts')
     end
   end
 end
