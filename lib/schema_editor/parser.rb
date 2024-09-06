109aa7605e77d7ebef95c41e1566b821ed0a3fcd
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-06 20:58:58 +0200

    logic: add foreign keys in extraction

--- lib/schema_editor/parser.rb
+++ lib/schema_editor/parser.rb
@@ -4,16 +4,31 @@ module SchemaEditor
 
     def extract(schema)
       data = parse_tables(schema)
+      foreign_keys = parse_foreign_keys(schema)
       sorted_data = data.sort_by do |key, _value|
-        -(parse_foreign_keys(schema)[key] || 0)
+        -(foreign_keys[key] || 0)
       end.to_h
-      sorted_data
+      { tables: sorted_data, foreign_keys: foreign_keys.keys }
+    end
+
+    def parse_foreign_keys(schema)
+      references = {}
+      filtered_content = schema.scan(/add_foreign_key "(\w+)", "(\w+)"/)
+      filtered_content.each do |table|
+        next if table[0].match?("active_storage")
+
+        references.has_key?(table[1]) ? references[table[1]] += 1 : references[table[1]] = 1
+      end
+      references
     end
 
     def parse_tables(schema)
       tables = {}
-      filtered_content = schema.scan(/create_table "(\w+)", .*? do \|t\|([\s\S]*?)end/m)
+      regex = /create_table\s+"(\w+)",\s+force:\s*:cascade\s+do\s+\|t\|\s+([\s\S]*?)\bend\b/m
+      filtered_content = schema.scan(regex)
       filtered_content.each do |table_name, table_body|
+        next if table_name.match?("active_storage")
+
         columns = {}
         table_body.scan(/t\.(\w+) "(\w+)"/).each do |type, name|
           columns[name] = type
@@ -21,19 +36,6 @@ module SchemaEditor
         tables[table_name] = columns
       end
       tables
-    end
-
-    def parse_foreign_keys(schema)
-      references = {}
-      filtered_content = schema.scan(/add_foreign_key "(\w+)", "(\w+)"/)
-      filtered_content.each do |table|
-        if references.has_key?(table[1])
-          references[table[1]] += 1
-        else
-          references[table[1]] = 1
-        end
-      end
-      references
     end
 
     private_class_method(:parse_tables, :parse_foreign_keys)
