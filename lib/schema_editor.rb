ff181bd6ed53f835192582c2a20018ab7e30e30e
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-06 12:13:54 +0200

    logic: pull all files

--- lib/schema_editor.rb
+++ lib/schema_editor.rb
@@ -1,5 +1,7 @@
 require "schema_editor/version"
 require "schema_editor/engine"
+require "schema_editor/parser"
+require "schema_editor/references_finder"
 
 module SchemaEditor
   # Your code goes here...
