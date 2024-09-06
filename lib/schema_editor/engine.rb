892ae2cb6759a4b85e1e2c336f8cd706047d9f77
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-06 20:57:21 +0200

    logic: initialize assets

--- lib/schema_editor/engine.rb
+++ lib/schema_editor/engine.rb
@@ -1,5 +1,9 @@
 module SchemaEditor
   class Engine < ::Rails::Engine
     isolate_namespace SchemaEditor
+
+    initializer "schema_editor.assets.precompile" do |app|
+      app.config.assets.precompile += %w(schema_editor/application.css schema_editor/table.js schema_editor/references.js)
+    end
   end
 end
