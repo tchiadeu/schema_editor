410e721f015beaefc87afc552fed8a610904dff0
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-06 20:56:57 +0200

    fix: exclude column that are not foreing keys

--- lib/schema_editor/references_finder.rb
+++ lib/schema_editor/references_finder.rb
@@ -2,10 +2,12 @@ module SchemaEditor
   module ReferencesFinder
     module_function
 
-    def filter(tables)
+    def filter(tables, foreign_keys)
       references = []
       tables.each do |_table, value|
         value.keys.each do |key|
+          next unless key.match?(/_id/) && foreign_keys.include?(Formatter.transform(key))
+
           references << key if key.match?(/_id/)
         end
       end
