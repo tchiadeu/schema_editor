0351c494a536e57233e7cb9cbb56da127478cc5b
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-06 12:14:21 +0200

    feature: add references

--- app/controllers/schema_editor/schemas_controller.rb
+++ app/controllers/schema_editor/schemas_controller.rb
@@ -1,11 +1,22 @@
-require "schema_editor/parser"
+require "schema_editor"
 
 module SchemaEditor
   class SchemasController < ApplicationController
     def index
+      @schema = Parser.extract(set_file)
+      @references = ReferencesFinder.filter(@schema).map { |column| replace_id(column) }
+    end
+
+    def replace_id(column)
+      column.gsub("_id", "s")
+    end
+    helper_method :replace_id
+
+    private
+
+    def set_file
       schema_path = Rails.root.join("spec", "fixtures", "schema.rb")
-      schema_content = File.read(schema_path)
-      @schema = Parser.extract(schema_content)
+      File.read(schema_path)
     end
   end
 end
