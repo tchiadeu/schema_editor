6a93f42606d449043604efbae1d229fd4a0dabeb
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-10 20:11:23 +0200

    silo: positions/create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SchemaEditor::Engine.routes.draw do
   resources :schema, only: :index
   root to: "schemas#index"
+
+  resources "positions", only: :create
+  # post "schema_editor/positions", to: "positions#update_positions"
 end
