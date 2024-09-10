3e91f1ee1f7ca3c885aae9fef2124457dde8ea6e
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2024-09-10 20:11:12 +0200

    feature: include new classes

--- lib/schema_editor.rb
+++ lib/schema_editor.rb
@@ -1,9 +1,7 @@
-require "schema_editor/version"
+require "schema_editor/config_parser"
+require "schema_editor/config_writter"
+require "schema_editor/db_parser"
 require "schema_editor/engine"
 require "schema_editor/formatter"
-require "schema_editor/parser"
 require "schema_editor/references_finder"
-
-module SchemaEditor
-  # Your code goes here...
-end
+require "schema_editor/version"
